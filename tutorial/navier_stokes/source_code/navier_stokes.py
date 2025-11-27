03a322ffff90c75e348140f5f86a8d7db85e986e
ivan <iauyeung@nvidia.com>
2025-11-27 11:58:52 +0800

    Revise typo

--- tutorial/navier_stokes/source_code/navier_stokes.py
+++ tutorial/navier_stokes/source_code/navier_stokes.py
@@ -42,7 +42,7 @@ from physicsnemo.sym.utils.io import (
 )
 
 def read_wf_data(velocity_scale,pressure_scale):
-    path = "./data_lat.npy"
+    path = os.path.join(os.getcwd(), "../../data_lat.npy")
     print(path)
     ic = np.load(path).astype(np.float32)
     
